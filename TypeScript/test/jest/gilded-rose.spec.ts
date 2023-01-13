eaa201e49dd40f887e4253fa15f592c60d8794a4
Suresh Kumar <extern.Suresh.Kumar@vw.com>
2023-01-13 12:57:41 -0500

    Updated existing and added new test cases for jest and mocha

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -4,6 +4,19 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+    expect(items[0].name).toBe('foo');
   });
+
+  it('should execute else clause', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('Aged Brie');    
+  });
+
+  it('Test for: "Backstage passes to a TAFKAL80ETC concert" text', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe('Backstage passes to a TAFKAL80ETC concert');    
+  });
+  
 });
--- TypeScript/test/mocha/gilded-rose.spec.ts
+++ TypeScript/test/mocha/gilded-rose.spec.ts
@@ -5,6 +5,19 @@ describe('Gilded Rose', () => {
   it('should foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal('fixme');
+    expect(items[0].name).to.equal('foo');
   });
+
+
+  it('should execute else clause', () => {
+    const gildedRose = new GildedRose([new Item('Aged Brie', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('Aged Brie');    
+  });
+
+  it('Test for: "Backstage passes to a TAFKAL80ETC concert" text', () => {
+    const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');    
+  });  
 });
